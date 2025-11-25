2693097951c395d8ff8b353d5c801bdbd0150343
Emerson1423 <emersonaldahir2453@gmail.com>
2025-11-25 17:43:13 -0600

    4

--- vue.config.js
+++ vue.config.js
@@ -6,13 +6,27 @@ module.exports = defineConfig({
   
   // Asegurar que _redirects se copie a dist
   chainWebpack: config => {
-    config.plugin('copy').tap(([options]) => {
-      options[0].patterns.push({
-        from: 'public/_redirects',
-        to: '_redirects',
-        toType: 'file',
-      })
-      return [options]
+    config.plugin('copy').tap(args => {
+      // Verificar si args[0] existe y tiene patterns
+      if (args[0] && args[0].patterns) {
+        args[0].patterns.push({
+          from: 'public/_redirects',
+          to: '_redirects',
+          toType: 'file',
+        })
+      } else {
+        // Si no existe, crear la estructura
+        args[0] = {
+          patterns: [
+            {
+              from: 'public/_redirects',
+              to: '_redirects',
+              toType: 'file',
+            }
+          ]
+        }
+      }
+      return args
     })
   }
 })
